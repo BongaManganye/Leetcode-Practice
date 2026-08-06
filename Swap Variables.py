c032d99556f9376fb1f7adc30a9080fee01dd993
Bonga <108957720+BongaManganye@users.noreply.github.com>
2026-08-06 19:01:35 +0200

    Add script to swap two variables without temp variable
    Implement a Python script to swap two variables.

---
+++ Swap Variables.py
@@ -0,0 +1,10 @@
+# Swap Variables
+
+#Given two variables a = 10 and b = 20, swap their values without creating a temporary third variable, then print both.
+
+a = 10
+b = 20
+
+a,b = b,a
+
+print(f"a: {a}, b: {b}")
