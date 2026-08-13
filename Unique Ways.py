6fb291760231598e9602b2cd6218c38a037243fa
Bonga <108957720+BongaManganye@users.noreply.github.com>
2026-08-14 00:56:50 +0200

    Add uniquePaths method in Solution class
    Implement uniquePaths method to calculate unique paths in a grid.

---
+++ Unique Ways.py
@@ -0,0 +1,15 @@
+#Unique Ways
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+
+        for i in range(m - 1):
+            newRow = [1] * n
+            for j in range(n - 2, -1, -1):
+                newRow[j] = newRow[j + 1] + row[j]
+            row = newRow
+        return row[0]
+
+#Time: O(n * m)
+#Space: O(n)
