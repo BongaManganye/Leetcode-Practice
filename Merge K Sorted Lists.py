2cc487b551ffc9170b9625d04ad28f5b4c04fc9d
Bonga <108957720+BongaManganye@users.noreply.github.com>
2026-08-27 02:48:12 +0200

    Add mergeKLists function to merge K sorted lists
    Implement a solution to merge K sorted linked lists into one sorted linked list.

---
+++ Merge K Sorted Lists.py
@@ -0,0 +1,25 @@
+# Merge K Sorted Lists
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if not lists or len(lists) == 0:
+            return None
+
+        while len(lists) > 1:
+            mergedLists = []
+
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergeLists.append(self.mergeList(l1, l2))
+            lists = mergedLists
+        return lists[0]
+
+    def mergeList(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
